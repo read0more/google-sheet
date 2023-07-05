d3ac05c5f725040b88594b23786e0194f0255850
read0more <read0more@gmail.com>
2023-07-05 20:13:06 +0900

    migrate to vite

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import App from "./App";
 import "./I18nProvider";
 import { I18nProvider } from "./I18nProvider";
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
     <I18nProvider>
       <App />
     </I18nProvider>
   </React.StrictMode>,
-  document.getElementById("root")
 );
